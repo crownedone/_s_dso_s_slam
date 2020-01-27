edd583ff2b15699bd367c7e7a3bc74bcb3d2bdec
steffen.ehrle <steffen.ehrle@myestro.de>
2020-01-27 16:03:13 +0100

    update to actual versions

--- tools/make.bat
+++ tools/make.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cd /d "%~dp0"
-C:\CMake\bin\cmake.exe .. -G "Visual Studio 15 2017 Win64" -DBUILD_SHARED_LIBS=ON -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
+C:\CMake\bin\cmake.exe .. -G "Visual Studio 16 2019" -A x64 -DBUILD_SHARED_LIBS=ON -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
 cd /d "%~dp0"
 Pangolin.sln
 
