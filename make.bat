3e5b4de67ceb13d9165ccf243964e34e10e958b3
steffen.ehrle <steffen.ehrle@myestro.de>
2020-03-04 20:14:22 +0100

    fix startup

--- make.bat
+++ make.bat
@@ -10,7 +10,7 @@ C:\CMake\bin\cmake.exe .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FI
 ) Else if Exist "C:\Program Files\CMake\bin\cmake.exe" (
 "C:\Program Files\CMake\bin\cmake.exe .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake"
 )
-cd /d "%~dp0"
 DSO_SLAM.sln
+cd /d "%~dp0"
 
 pause
