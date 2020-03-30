4dd41d9f3ae456090509300da69a07ec187c080f
ANKIT NAGAR <ankitfriends007@gmail.com>
2020-03-30 21:07:05 +0200

    makebat path changes

--- make.bat
+++ make.bat
@@ -5,12 +5,17 @@ If Not Exist build (
 mkdir build
 )
 cd build
-If Exist C:\CMake\bin\cmake.exe  (
+
+If Exist "C:\CMake\bin\cmake.exe"  (
 C:\CMake\bin\cmake.exe .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
-) Else if Exist "C:\Program Files\CMake\bin\cmake.exe" (
-"C:\Program Files\CMake\bin\cmake.exe .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake"
+) Else If Exist 'C:\Program\ Files\CMake\bin\cmake.exe' (
+"C:\Program\ Files\CMake\bin\cmake.exe" .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
+) Else If Exist "D:\Program Files\CMake\bin\cmake.exe"  (
+"D:\Program Files\CMake\bin\cmake.exe" .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
+) Else If Exist 'D:\Program Files\CMake\bin\cmake.exe' (
+"D:\Program Files\CMake\bin\cmake.exe" .. -G "Visual Studio 16 2019" -A x64 -DCMAKE_TOOLCHAIN_FILE=C:/vcpkg/scripts/buildsystems/vcpkg.cmake
 )
 DSO_SLAM.sln
-cd /d "%~dp0"
+cd /d "%~dp0
 
 pause
